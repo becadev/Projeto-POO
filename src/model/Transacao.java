a40ad999142018cb8cb5fe9c0008a1b89a1e65cb
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-02-15 18:57:26 -0300

    Delete src directory

--- src/model/Transacao.java
+++
@@ -1,9 +0,0 @@
-// interface para receitas e despesas
-package src.model;
-
-interface Transacao{
-    double getValor();
-    String getDescricao();
-    String getData();
-    String getCategoria();
-}
